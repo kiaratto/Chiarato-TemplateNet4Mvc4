ff4b5e541fafcd8920461a4fc4c7db57cdbb4e3c
Rodrigo Chiarato Domingues <rodrigochiarato@gmail.com>
2017-06-26 20:27:35 -0300

    Segundo Commit -> Build OK

---
+++ Chiarato-TemplateNet4Mvc4/Chiarato-TemplateNet4Mvc4/Controller/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplicationVazioNet4Mvc4.ViewModel;
+
+namespace WebApplicationVazioNet4Mvc4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new HomeViewModel() { Titulo = "Aaaaaa" });
+        }
+       
+    }
+}
---
+++ Chiarato-TemplateNet4Mvc4/Chiarato-TemplateNet4Mvc4/Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplicationVazioNet4Mvc4
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // configure mvc controllers
+            RouteTable.Routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
